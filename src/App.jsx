b83e4ef2b6c05bf292fa02646b42cf3998e3310a
shoaib-sahil <mianshaibi6@gmail.com>
2025-01-06 04:06:05 +0500

    Create Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-
-
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Projects from './pages/Projects';
 
 function App() {
  
 
   return (
     <>
-   <h1>Welcome To Sahil Blog </h1>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      welcome to the home page
+    </div>
+  )
+}
+
+export default Home
